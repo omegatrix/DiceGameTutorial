7433d759650f193222db4298549397395f466296
omegatrix <arnoldbronson16@gmail.com>
2018-03-25 21:42:30 +0100

    Added some variables

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice Math.floor(Math.random() *6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//document.querySelector('current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
